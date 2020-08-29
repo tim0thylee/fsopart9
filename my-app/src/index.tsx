aeb3d0f8cabe1a0bd6946937584a68de069581da
Timothy Lee <timothylee43@gmail.com>
2020-08-29 15:08:37 -0700

    completed one part of 9.15

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -4,20 +4,52 @@ import Header from './components/Header';
 import Content from './components/Content';
 import Total from './components/Total';
 
+
 const App: React.FC = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+  // new types
+  interface CoursePartBase {
+    name: string;
+    exerciseCount: number;
+  }
+
+  interface CoursePartBaseDescription extends CoursePartBase {
+    description: string;
+  }
+
+  interface CoursePartOne extends CoursePartBaseDescription  {
+    name: "Fundamentals";
+  }
+
+  interface CoursePartTwo extends CoursePartBase {
+    name: "Using props to pass data";
+    groupProjectCount: number;
+  }
+
+  interface CoursePartThree extends CoursePartBaseDescription  {
+    name: "Deeper type usage";
+    exerciseSubmissionLink: string;
+  }
+
+  type CoursePart = CoursePartOne | CoursePartTwo | CoursePartThree;
+
+  // this is the new coursePart variable
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is an awesome course part"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: "Confusing description",
+      exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev"
     }
   ];
 
